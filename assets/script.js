2b59fa11b9af00d14a4586fd620550b683382c45
Thefany Silva <thefanyja@gmail.com>
2022-03-03 15:49:41 -0500

    modal created for projects to display when clicked. Created script tags for external file and jquery

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('#open').click(function(){
+$('#myModal').fadeIn('slow');
+    });
+    $("#close").click(function(){
+        $('#myModal').fadeOut('slow');
+
+    });
+  });
